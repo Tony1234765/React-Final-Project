a47c6f6fdd4169273473a485aeb71e77e56658b5
Tony1234765 <perezflorescarlosantonio@gmail.com>
2024-10-17 22:21:47 -0600

    Added and modified all  necessary files

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((items) => items.name == name);
+      if (existingItem) {
+        existingItem.updateQuantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const actualItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.name === actualItem.name
+      );
+      if (existingItem) {
+        state.items = state.items.filter(
+          (item) => item.name !== actualItem.name
+        );
+      }
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+      if (itemToUpdate.quantity === 0) {
+        state.items = state.items.filter((item) => item.name !== name);
+      }
     },
   },
 });
